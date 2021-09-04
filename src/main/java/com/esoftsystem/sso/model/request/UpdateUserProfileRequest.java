fa6e8a6eae2fe27714446d5181a9ba92451855d3
Vinh Nguyen The <vinh.nth@esoft.com>
2021-09-04 18:49:05 +0700

    CSD-3021 Change the old password field to current password

--- src/main/java/com/esoftsystem/sso/model/request/UpdateUserProfileRequest.java
+++ src/main/java/com/esoftsystem/sso/model/request/UpdateUserProfileRequest.java
@@ -6,6 +6,8 @@
  */
 package com.esoftsystem.sso.model.request;
 
+import java.util.Date;
+
 import com.esoftsystem.sso.model.request.base.UpdateUserBaseRequest;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -21,7 +23,6 @@ import lombok.NoArgsConstructor;
  * @since 1.0.0
  */
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
@@ -29,6 +30,31 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UpdateUserProfileRequest extends UpdateUserBaseRequest {
 
-  private String oldPassword;
+  private String currentPassword;
   private String newPassword;
+
+  /**
+   * All Args Constructor.
+   *
+   * @param title           user's title
+   * @param firstName       user's first name
+   * @param middleName      user's middle name
+   * @param lastName        user's last name
+   * @param dateOfBirth     user's date of birth
+   * @param avatarUrl       user's avatar
+   * @param skypeId         user's skypeId
+   * @param countryCode     user's country code
+   * @param phone           user's phone number
+   * @param currentPassword submitted current password for checking before changing to new password.
+   * @param newPassword     new password to change.
+   */
+  //CHECKSTYLE.OFF: ParameterNumber
+  @Builder
+  public UpdateUserProfileRequest(String title, String firstName, String middleName, String lastName, Date dateOfBirth,
+                                  String avatarUrl, String skypeId, String countryCode, String phone, String currentPassword, String newPassword) {
+    super(title, firstName, middleName, lastName, dateOfBirth, avatarUrl, skypeId, countryCode, phone);
+    this.currentPassword = currentPassword;
+    this.newPassword = newPassword;
+  }
+  //CHECKSTYLE.ON: ParameterNumber
 }
