b363b333cbf283ac8d94b7227f079f05f0d0a352
Vinh Nguyen The <vinh.nth@esoft.com>
2021-09-19 01:01:06 +0700

    Create UserSummaryDto & UserFilterRequest

---
+++ src/main/java/com/esoftsystem/sso/model/UserSummaryDto.java
@@ -0,0 +1,73 @@
+/*
+ * @(#)UserSummaryDto.java
+ *
+ * Copyright 2021 by esoft systems (tm).
+ * All rights reserved.
+ */
+package com.esoftsystem.sso.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Vinh Nguyen
+ * @since 1.0.0
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserSummaryDto implements Serializable {
+
+  private static final long serialVersionUID = 3088854471808668803L;
+  @JsonProperty("userId")
+  private Long userId;
+  @JsonProperty("username")
+  private String username;
+  @JsonProperty("resourceName")
+  private String resourceName;
+  @JsonProperty("title")
+  private String title;
+  @JsonProperty("firstName")
+  private String firstName;
+  @JsonProperty("middleName")
+  private String middleName;
+  @JsonProperty("lastName")
+  private String lastName;
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  @JsonProperty("dateOfBirth")
+  private Date dateOfBirth;
+  @JsonProperty("avatarUrl")
+  private String avatarUrl;
+  @JsonProperty("status")
+  private String status;
+  @JsonProperty("enabled")
+  private Boolean enabled;
+  @JsonProperty(value = "skypeId")
+  private String skypeId;
+  @JsonProperty(value = "countryCode")
+  private String countryCode;
+  @JsonProperty(value = "phone")
+  private String phone;
+  @JsonProperty(value = "team")
+  private TeamDto team;
+  @JsonProperty(value = "company")
+  private CompanyDto company;
+  @JsonProperty(value = "location")
+  private LocationDto location;
+  @JsonProperty("groups")
+  private List<UserGroupDto> groups;
+}
---
+++ src/main/java/com/esoftsystem/sso/model/request/FindUserSummaryRequest.java
@@ -0,0 +1,49 @@
+/*
+ * @(#)FindUserSummaryRequest.java
+ *
+ * Copyright 2021 by esoft systems (tm).
+ * All rights reserved.
+ */
+package com.esoftsystem.sso.model.request;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Vinh Nguyen
+ * @since 1.0.0
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FindUserSummaryRequest implements Serializable {
+
+  private static final long serialVersionUID = -8748598355778971407L;
+  @JsonProperty(value = "teamCodes")
+  private List<String> teamCodes;
+  @JsonProperty(value = "locations")
+  private List<String> locations;
+  @JsonProperty(value = "groupIds")
+  private List<Long> groupIds;
+  @JsonProperty(value = "statuses")
+  private List<String> statuses;
+  @JsonProperty(value = "keywords")
+  private String keywords;
+
+  @JsonProperty(value = "pageIndex")
+  private Integer pageIndex;
+  @JsonProperty(value = "pageSize")
+  private Integer pageSize;
+}
