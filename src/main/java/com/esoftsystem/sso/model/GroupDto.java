57b4f42ef8cc82273fc5d56a92ccacfe40706db7
Vinh Nguyen The <vinh.nth@esoft.com>
2021-09-19 13:52:32 +0700

    Add GroupDto

---
+++ src/main/java/com/esoftsystem/sso/model/GroupDto.java
@@ -0,0 +1,38 @@
+/*
+ * @(#)TeamDto.java
+ *
+ * Copyright 2021 by esoft systems (tm).
+ * All rights reserved.
+ */
+package com.esoftsystem.sso.model;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Vinh Nguyen
+ * @since 1.0
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GroupDto implements Serializable {
+
+  private static final long serialVersionUID = -5672124139056221361L;
+  @JsonProperty("groupId")
+  private Long groupId;
+
+  @JsonProperty("groupName")
+  private String groupName;
+}
