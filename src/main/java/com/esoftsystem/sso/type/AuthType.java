9027b774c72cf147d15b952fbf9ea914a326b526
vu.nga <vu.nga@esoft.com>
2021-10-12 10:56:15 +0700

    CSD-3333: PJS: Create API for creating and updating user

---
+++ src/main/java/com/esoftsystem/sso/type/AuthType.java
@@ -0,0 +1,35 @@
+/*
+ * @(#)AuthTypeEnum.java
+ *
+ * Copyright 2021 by esoft systems (tm).
+ * All rights reserved.
+ */
+package com.esoftsystem.sso.type;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * This class holds authorities.
+ *
+ * @author Chien Nguyen Tien
+ * @since 1.0
+ *
+ */
+public class AuthType {
+
+  public static final String ROLE_PRODUCTION_JOB_USER = "ROLE_PRODUCTION_JOB_USER";
+  public static final String ROLE_PRODUCTION_JOB_ADMIN = "ROLE_PRODUCTION_JOB_ADMIN";
+  public static final String ROLE_PRODUCTION_JOB_PROJECT_MANAGER = "ROLE_PRODUCTION_JOB_PROJECT_MANAGER";
+  public static final String ROLE_PRODUCTION_JOB_SUPERVISOR = "ROLE_PRODUCTION_JOB_SUPERVISOR";
+  public static final String ROLE_PRODUCTION_JOB_TEAM_LEADER = "ROLE_PRODUCTION_JOB_TEAM_LEADER";
+  public static final String ROLE_PRODUCTION_JOB_PART_TIMER = "ROLE_PRODUCTION_JOB_PART_TIMER";
+
+  public static List<String> getUserRoles() {
+    return Arrays.asList(ROLE_PRODUCTION_JOB_USER, ROLE_PRODUCTION_JOB_ADMIN);
+  }
+
+  public static List<String> getAminRoles() {
+    return Arrays.asList(ROLE_PRODUCTION_JOB_USER, ROLE_PRODUCTION_JOB_ADMIN);
+  }
+}
