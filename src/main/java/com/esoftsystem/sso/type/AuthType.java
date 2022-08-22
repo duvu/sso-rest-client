9474771e01a233ca115582425f816bc72a10bfd3
Vu D <hoaivubk@gmail.com>
2022-08-22 17:42:21 +0700

    CSD-4399 update role-based permissions

--- src/main/java/com/esoftsystem/sso/type/AuthType.java
+++ src/main/java/com/esoftsystem/sso/type/AuthType.java
@@ -25,6 +25,43 @@ public class AuthType {
   public static final String ROLE_PRODUCTION_JOB_TEAM_LEADER = "ROLE_PRODUCTION_JOB_TEAM_LEADER";
   public static final String ROLE_PRODUCTION_JOB_PART_TIMER = "ROLE_PRODUCTION_JOB_PART_TIMER";
 
+  public static final String ROLE_USER_MANAGEMENT_LIST_VIEW        = "ROLE_USER_MANAGEMENT_LIST_VIEW";
+  public static final String ROLE_USER_MANAGEMENT_LIST_MANAGE      = "ROLE_USER_MANAGEMENT_LIST_MANAGE";
+  public static final String ROLE_USER_MANAGEMENT_LIST_ADMIN       = "ROLE_USER_MANAGEMENT_LIST_ADMIN";
+  public static final String ROLE_USER_MANAGEMENT_ADD_EDIT_VIEW    = "ROLE_USER_MANAGEMENT_ADD_EDIT_VIEW";
+  public static final String ROLE_USER_MANAGEMENT_ADD_EDIT_MANAGE  = "ROLE_USER_MANAGEMENT_ADD_EDIT_MANAGE";
+  public static final String ROLE_USER_MANAGEMENT_ADD_EDIT_ADMIN   = "ROLE_USER_MANAGEMENT_ADD_EDIT_ADMIN";
+  public static final String ROLE_P3_SETTINGS_VIEW                 = "ROLE_P3_SETTINGS_VIEW";
+  public static final String ROLE_P3_SETTINGS_MANAGE               = "ROLE_P3_SETTINGS_MANAGE";
+  public static final String ROLE_P3_SETTINGS_ADMIN                = "ROLE_P3_SETTINGS_ADMIN";
+  public static final String ROLE_JOB_OVERVIEW_VIEW                = "ROLE_JOB_OVERVIEW_VIEW";
+  public static final String ROLE_JOB_OVERVIEW_MANAGE              = "ROLE_JOB_OVERVIEW_MANAGE";
+  public static final String ROLE_JOB_OVERVIEW_ADMIN               = "ROLE_JOB_OVERVIEW_ADMIN";
+  public static final String ROLE_USER_FORM_LIST_VIEW              = "ROLE_USER_FORM_LIST_VIEW";
+  public static final String ROLE_USER_FORM_LIST_MANAGE            = "ROLE_USER_FORM_LIST_MANAGE";
+  public static final String ROLE_USER_FORM_LIST_ADMIN             = "ROLE_USER_FORM_LIST_ADMIN";
+  public static final String ROLE_USER_FORM_ADD_VIEW               = "ROLE_USER_FORM_ADD_VIEW";
+  public static final String ROLE_USER_FORM_ADD_MANAGE             = "ROLE_USER_FORM_ADD_MANAGE";
+  public static final String ROLE_USER_FORM_ADD_ADMIN              = "ROLE_USER_FORM_ADD_ADMIN";
+  public static final String ROLE_USER_FORM_EDIT_VIEW              = "ROLE_USER_FORM_EDIT_VIEW";
+  public static final String ROLE_USER_FORM_EDIT_MANAGE            = "ROLE_USER_FORM_EDIT_MANAGE";
+  public static final String ROLE_USER_FORM_EDIT_ADMIN             = "ROLE_USER_FORM_EDIT_ADMIN";
+  public static final String ROLE_USER_FORM_PREVIEW_VIEW           = "ROLE_USER_FORM_PREVIEW_VIEW";
+  public static final String ROLE_USER_FORM_PREVIEW_MANAGE         = "ROLE_USER_FORM_PREVIEW_MANAGE";
+  public static final String ROLE_USER_FORM_PREVIEW_ADMIN          = "ROLE_USER_FORM_PREVIEW_ADMIN";
+  public static final String ROLE_PRODUCT_UPDATE_LIST_VIEW         = "ROLE_PRODUCT_UPDATE_LIST_VIEW";
+  public static final String ROLE_PRODUCT_UPDATE_LIST_MANAGE       = "ROLE_PRODUCT_UPDATE_LIST_MANAGE";
+  public static final String ROLE_PRODUCT_UPDATE_LIST_ADMIN        = "ROLE_PRODUCT_UPDATE_LIST_ADMIN";
+  public static final String ROLE_PRODUCT_UPDATE_ADD_EDIT_VIEW     = "ROLE_PRODUCT_UPDATE_ADD_EDIT_VIEW";
+  public static final String ROLE_PRODUCT_UPDATE_ADD_EDIT_MANAGE   = "ROLE_PRODUCT_UPDATE_ADD_EDIT_MANAGE";
+  public static final String ROLE_PRODUCT_UPDATE_ADD_EDIT_ADMIN    = "ROLE_PRODUCT_UPDATE_ADD_EDIT_ADMIN";
+  public static final String ROLE_PRODUCT_UPDATE_VIEW_VIEW         = "ROLE_PRODUCT_UPDATE_VIEW_VIEW";
+  public static final String ROLE_PRODUCT_UPDATE_VIEW_MANAGE       = "ROLE_PRODUCT_UPDATE_VIEW_MANAGE";
+  public static final String ROLE_PRODUCT_UPDATE_VIEW_ADMIN        = "ROLE_PRODUCT_UPDATE_VIEW_ADMIN";
+  public static final String ROLE_PRODUCT_UPDATE_PREVIEW_VIEW      = "ROLE_PRODUCT_UPDATE_PREVIEW_VIEW";
+  public static final String ROLE_PRODUCT_UPDATE_PREVIEW_MANAGE    = "ROLE_PRODUCT_UPDATE_PREVIEW_MANAGE";
+  public static final String ROLE_PRODUCT_UPDATE_PREVIEW_ADMIN     = "ROLE_PRODUCT_UPDATE_PREVIEW_ADMIN";
+
   public static final List<String> TEAM_ADMIN_ROLES = Arrays.asList(AuthType.ROLE_PRODUCTION_JOB_ADMIN,
                                                                     AuthType.ROLE_PRODUCTION_JOB_PRODUCT_MANAGER,
                                                                     AuthType.ROLE_PRODUCTION_JOB_SUPERVISOR,
