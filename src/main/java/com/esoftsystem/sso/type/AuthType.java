0a484e6210226606eae4c297eb3edad0c22e03ef
Vu D <hoaivubk@gmail.com>
2022-09-09 13:31:39 +0700

    CSD-4446 Changed P_3 to P3 and up version

--- src/main/java/com/esoftsystem/sso/type/AuthType.java
+++ src/main/java/com/esoftsystem/sso/type/AuthType.java
@@ -16,19 +16,20 @@ import java.util.List;
  * @since 1.0
  */
 public class AuthType {
-  public static final String ROLE_SYSTEM_ADMINISTRATOR = "ROLE_SYSTEM_ADMINISTRATOR";
-  public static final String ROLE_PRODUCTION_JOB_USER = "ROLE_PRODUCTION_JOB_USER";
-  public static final String ROLE_PRODUCTION_JOB_ADMIN = "ROLE_PRODUCTION_JOB_ADMIN";
+  public static final String ROLE_SYSTEM_ADMINISTRATOR                = "ROLE_SYSTEM_ADMINISTRATOR";
+  public static final String ROLE_PRODUCTION_JOB_USER                 = "ROLE_PRODUCTION_JOB_USER";
+  public static final String ROLE_PRODUCTION_JOB_ADMIN                = "ROLE_PRODUCTION_JOB_ADMIN";
 
-  public static final String ROLE_PRODUCTION_JOB_PRODUCT_MANAGER = "ROLE_PRODUCTION_JOB_PRODUCT_MANAGER";
-  public static final String ROLE_PRODUCTION_JOB_SUPERVISOR = "ROLE_PRODUCTION_JOB_SUPERVISOR";
-  public static final String ROLE_PRODUCTION_JOB_TEAM_LEADER = "ROLE_PRODUCTION_JOB_TEAM_LEADER";
-  public static final String ROLE_PRODUCTION_JOB_PART_TIMER = "ROLE_PRODUCTION_JOB_PART_TIMER";
+  public static final String ROLE_PRODUCTION_JOB_PRODUCT_MANAGER      = "ROLE_PRODUCTION_JOB_PRODUCT_MANAGER";
+  public static final String ROLE_PRODUCTION_JOB_SUPERVISOR           = "ROLE_PRODUCTION_JOB_SUPERVISOR";
+  public static final String ROLE_PRODUCTION_JOB_TEAM_LEADER          = "ROLE_PRODUCTION_JOB_TEAM_LEADER";
+  public static final String ROLE_PRODUCTION_JOB_PART_TIMER           = "ROLE_PRODUCTION_JOB_PART_TIMER";
 
   public static final String ROLE_ESOFT_USER                          = "ROLE_ESOFT_USER";
   public static final String ROLE_PRODUCTION_JOB_EDITOR               = "ROLE_PRODUCTION_JOB_EDITOR";
   public static final String ROLE_PRODUCTION_JOB_QC                   = "ROLE_PRODUCTION_JOB_QC";
   public static final String ROLE_PRODUCTION_JOB_INTERNATIONAL_USER   = "ROLE_PRODUCTION_JOB_INTERNATIONAL_USER";
+
   public static final String ROLE_USER_MANAGEMENT_LIST_VIEW           = "ROLE_USER_MANAGEMENT_LIST_VIEW";
   public static final String ROLE_USER_MANAGEMENT_LIST_MANAGE         = "ROLE_USER_MANAGEMENT_LIST_MANAGE";
   public static final String ROLE_USER_MANAGEMENT_LIST_ADMIN          = "ROLE_USER_MANAGEMENT_LIST_ADMIN";
@@ -41,9 +42,9 @@ public class AuthType {
   public static final String ROLE_USER_PROFILE_VIEW                   = "ROLE_USER_PROFILE_VIEW";
   public static final String ROLE_USER_PROFILE_MANAGE                 = "ROLE_USER_PROFILE_MANAGE";
   public static final String ROLE_USER_PROFILE_ADMIN                  = "ROLE_USER_PROFILE_ADMIN";
-  public static final String ROLE_P_3_SETTINGS_VIEW                   = "ROLE_P_3_SETTINGS_VIEW";
-  public static final String ROLE_P_3_SETTINGS_MANAGE                 = "ROLE_P_3_SETTINGS_MANAGE";
-  public static final String ROLE_P_3_SETTINGS_ADMIN                  = "ROLE_P_3_SETTINGS_ADMIN";
+  public static final String ROLE_P3_SETTINGS_VIEW                    = "ROLE_P3_SETTINGS_VIEW";
+  public static final String ROLE_P3_SETTINGS_MANAGE                  = "ROLE_P3_SETTINGS_MANAGE";
+  public static final String ROLE_P3_SETTINGS_ADMIN                   = "ROLE_P3_SETTINGS_ADMIN";
   public static final String ROLE_USER_FEEDBACK_FORM_VIEW             = "ROLE_USER_FEEDBACK_FORM_VIEW";
   public static final String ROLE_USER_FEEDBACK_FORM_MANAGE           = "ROLE_USER_FEEDBACK_FORM_MANAGE";
   public static final String ROLE_USER_FEEDBACK_FORM_ADMIN            = "ROLE_USER_FEEDBACK_FORM_ADMIN";
